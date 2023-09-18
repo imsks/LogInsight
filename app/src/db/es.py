9df4f74600991ae9ab432700bfa5b83aaee09431
Sachin <sachinkshuklaoo7@gmail.com>
2023-09-18 13:21:48 +0530

    Flash COnnect OS issue

--- app/src/db/es.py
+++ app/src/db/es.py
@@ -1,10 +1,32 @@
 from opensearchpy import OpenSearch
 
-es_client = OpenSearch(
-    hosts = [{"host": "localhost", "port": 9200}],
-    http_auth = ("admin", "admin"),
-    use_ssl = True,
-    verify_certs = False,
-    ssl_assert_hostname = False,
-    ssl_show_warn = False,
-)
+# es_client = OpenSearch(
+#     hosts = [{"host": "localhost", "port": 9200}],
+#     http_auth = ("admin", "admin"),
+#     use_ssl = True,
+#     verify_certs = False,
+#     ssl_assert_hostname = False,
+#     ssl_show_warn = False,
+# )
+
+def connect_to_os():
+    host = 'opensearch-node1'
+    port = 9200
+    auth = ('admin', 'admin')
+
+    try:
+        client = OpenSearch(
+            hosts = [{'host': host, 'port': port}],
+            http_auth=auth,
+            http_compress=True,
+            use_ssl=True,
+            verify_certs=False,
+            ssl_assert_hostname=False,
+            ssl_show_warn=False,
+        )
+        print("HERE", client)
+    except BaseException: 
+        print("ERROR unable to connect opensearch instance")
+        return False
+    
+    return client
--- app/src/db/indexing.py
+++ app/src/db/indexing.py
@@ -1,4 +1,4 @@
-from .es import es_client
+from .es import connect_to_os
 from utils.functions import get_mapped_entries
 
 index_name = "logs"
@@ -16,16 +16,17 @@ def create_mapping():
     }
 
     try:
-        es_client.indices.create(index_name, body=body)
-        print("Mapping created")
-    except:
-        print(f"Error in Creating Mapping {index_name}")
+        connect_to_os().indices.create(index_name, body=body)
+        return True
+    except Exception as e:
+        print(f"Error in Creating Mapping {index_name} {e}")
+        return False
 
 # Add Docs in Mapping
 def add_docs(index_name, entries):
     for entry in entries:
         try:
-            es_client.index(index=index_name, body=entry)
+            connect_to_os.index(index=index_name, body=entry)
             print(f"Document added in {index_name} as {entry}")
         except:
             print(f"Error in adding the {entry} document as {index_name}")
@@ -33,7 +34,7 @@ def add_docs(index_name, entries):
 # Delete Mapping
 def delete_mapping(index_name):
     try:
-        es_client.indices.delete(index=index_name)
+        connect_to_os.indices.delete(index=index_name)
         print("Mappings deleted!")
     except:
         print(f"Error in deleting the document as {index_name}")
